06eb8b9d3e40aead4d1477de2b1fe3d744517708
linefire <sasha.lada5@gmail.com>
2018-05-08 17:22:10 +0300

    antgame_l4_v2

---
+++ AntGame/MainForm.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace AntGame
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.goButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelNum = new System.Windows.Forms.Label();
+            this.labelEat = new System.Windows.Forms.Label();
+            this.trackBarSpeed = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // goButton
+            // 
+            this.goButton.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.goButton.Location = new System.Drawing.Point(516, 660);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(138, 29);
+            this.goButton.TabIndex = 0;
+            this.goButton.Text = "Вперед муравей";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(642, 642);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // labelNum
+            // 
+            this.labelNum.AutoSize = true;
+            this.labelNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelNum.Location = new System.Drawing.Point(12, 664);
+            this.labelNum.Name = "labelNum";
+            this.labelNum.Size = new System.Drawing.Size(57, 20);
+            this.labelNum.TabIndex = 2;
+            this.labelNum.Text = "Ход: 0";
+            // 
+            // labelEat
+            // 
+            this.labelEat.AutoSize = true;
+            this.labelEat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelEat.Location = new System.Drawing.Point(86, 664);
+            this.labelEat.Name = "labelEat";
+            this.labelEat.Size = new System.Drawing.Size(140, 20);
+            this.labelEat.TabIndex = 3;
+            this.labelEat.Text = "Собрано яблок: 0";
+            // 
+            // trackBarSpeed
+            // 
+            this.trackBarSpeed.LargeChange = 100;
+            this.trackBarSpeed.Location = new System.Drawing.Point(330, 660);
+            this.trackBarSpeed.Maximum = 2000;
+            this.trackBarSpeed.Minimum = 50;
+            this.trackBarSpeed.Name = "trackBarSpeed";
+            this.trackBarSpeed.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.trackBarSpeed.Size = new System.Drawing.Size(168, 45);
+            this.trackBarSpeed.SmallChange = 100;
+            this.trackBarSpeed.TabIndex = 4;
+            this.trackBarSpeed.TickFrequency = 200;
+            this.trackBarSpeed.Value = 500;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(243, 664);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Скорость";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 695);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBarSpeed);
+            this.Controls.Add(this.labelEat);
+            this.Controls.Add(this.labelNum);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.goButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Игра \"Умный муравей\"";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelNum;
+        private System.Windows.Forms.Label labelEat;
+        private System.Windows.Forms.TrackBar trackBarSpeed;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AntGame/MainForm.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AntGame
+{
+    public partial class MainForm : Form
+    {
+        int[,] tor, defaultTor ={{0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},  //ТОР 1-яблоко 0-пусто
+                                 {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {1,1,1,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1},
+                                 {0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+                                 {0,0,0,1,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+                                 {0,0,0,1,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+                                 {0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+                                 {0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,1,0,0,1,1,1,1,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                 {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+
+        public MainForm() //Конструтор главной формы
+        {
+            InitializeComponent();
+            tor = (int[,])defaultTor.Clone(); //клонирем тор (нужно для сброса с будущем)
+        }
+
+        private void PaintTor(Graphics g) //Функция отрисовки тора
+        {
+            try
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality; //Отрисовка со зглаживанием
+                for (int i = 0; i < 32; i++) //пройтись по всем строкам
+                {
+                    for (int j = 0; j < 32; j++) //пройтись по всем столбцам
+                    {
+                        switch (tor[j, i]) 
+                        {
+                            case 0: //если 0 чистим клетку и рисуем квадрат
+                                g.FillRectangle(Brushes.White, 1 + i * 20, 1 + j * 20, 20, 20);
+                                g.DrawRectangle(Pens.Black, 1 + i * 20, 1 + j * 20, 20, 20);
+                                break;
+                            case 1: //если 1 рисуем запоненный квадрат
+                                g.FillRectangle(Brushes.Black, 1 + i * 20, 1 + j * 20, 20, 20);
+                                break;
+                        }
+                    }
+                }
+                Image img = Resource.ant; //изображение муравья
+                img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                g.DrawImage(img, new Point(1 + x * 20, 1 + y * 20));
+            }
+            catch (Exception) { };
+        }
+
+        int x = 0, y = 0; //координаты
+        int v = 0; //направление 0 - восток 1 - юг 2 запад 3 север
+        int state = 0; //состояние (генетический алгоритм)
+        int appleNum = 0; //количество собранных яблок
+        Thread thread; //поток в котором вычисляется путь муравья
+        bool work = false; //поток работает пока work true (нужно для кнопки сброс)
+
+        private void goButton_Click(object sender, EventArgs e) //нажатие на кнопку вызывает эту функцию
+        {
+            if (goButton.Text == "Вперед муравей")
+            {
+                goButton.Enabled = false; //выключение кнопки (во избежание повторных запусков)
+                PaintTor(pictureBox1.CreateGraphics()); //перерисовать тор
+                thread = new Thread(ThreadFunc); //создаем "поток размышлений муравья"
+                work = true; //работает
+                thread.Start(); //запускаем поток
+                goButton.Text = "Сброс"; //меняем текст кнопки
+                goButton.Enabled = true; //включение кнопки
+            }
+            else
+            {
+                goButton.Enabled = false; //выключение кнопки (во избежание повторных сбросов)
+                work = false; //для остановки потока
+                thread.Abort(); //останавливаем поток
+                thread.Join(); //ждем завершения работы потока
+                tor = (int[,])defaultTor.Clone(); //сбрасываем тор
+                appleNum = 1; //сбрасываем количество яблок
+                x = 0; //сбрасываем координаты и направление
+                y = 0;
+                v = 0;
+                PaintTor(pictureBox1.CreateGraphics()); //перерисовать тор
+                this.Invoke((MethodInvoker)delegate() { labelNum.Text = "Ход: 0"; }); //сбрасываем надписи в окне
+                this.Invoke((MethodInvoker)delegate() { labelEat.Text = "Собрано яблок: 0"; }); //сбрасываем надписи в окне
+                goButton.Text = "Вперед муравей"; //меняем текст кнопки
+                goButton.Enabled = true; //включение кнопки
+            }
+        }
+
+
+        private void ThreadFunc() //Функция потока
+        {
+            try //во избежание ошибок
+            {
+                tor = (int[,])defaultTor.Clone();  //сбрасываем
+                appleNum = 1;
+                x = 0;
+                y = 0;
+                v = 0;
+                state = 0;
+
+                for (int i = 0; i < 200 && work; i++)
+                {
+                    if (tor[y, x] == 1)
+                    { //если на клетке которой стоит муравей есть еда то
+                        tor[y, x] = 0; //съесть ее
+                        appleNum++; //считаем количество яблок
+                    }
+                    Graphics g = pictureBox1.CreateGraphics(); //нужно для отрисовки
+                    g.FillRectangle(Brushes.White, 1 + x * 20, 1 + y * 20, 20, 20); //чистим клетку на которой стоит муравей
+                    g.DrawRectangle(Pens.Black, 1 + x * 20, 1 + y * 20, 20, 20);
+                    this.Invoke((MethodInvoker)delegate() { labelNum.Text = "Ход: " + (i + 1); }); //обновляем надписи в окне
+                    this.Invoke((MethodInvoker)delegate() { labelEat.Text = "Собрано яблок: " + appleNum; });
+
+                    //ГЕНЕТИЧЕСКИЙ АЛГОРИТМ
+                    switch (state)
+                    {
+                        case 0:
+                            if (checkEat())
+                            {
+                                state = 0;
+                                go();
+                            }
+                            else
+                            {
+                                state = 1;
+                                right();
+                            }
+                            break;
+                        case 1:
+                            if (checkEat())
+                            {
+                                state = 12;
+                                go();
+                            }
+                            else
+                            {
+                                state = 2;
+                                right();
+                            }
+                            break;
+                        case 2:
+                            if (!checkEat())
+                            {
+                                state = 3;
+                                right();
+                            }
+                            break;
+                        case 3:
+                            if (checkEat())
+                            {
+                                state = 6;
+                                go();
+                            }
+                            else
+                            {
+                                state = 4;
+                                right();
+                            }
+                            break;
+                        case 4:
+                            if (checkEat())
+                            {
+                                state = 5;
+                                go();
+                            }
+                            else
+                            {
+                                state = 0;
+                                go();
+                            }
+                            break;
+                        case 5:
+                            if (checkEat())
+                            {
+                                state = 3;
+                                go();
+                            }
+                            break;
+                        case 6:
+                            if (checkEat())
+                                state = 7;
+                            else
+                            {
+                                state = 4;
+                                go();
+                            }
+                            break;
+                        case 7:
+                            if (checkEat())
+                                state = 9;
+                            else
+                            {
+                                state = 8;
+                                go();
+                            }
+                            break;
+                        case 8:
+                            if (!checkEat())
+                            {
+                                state = 3;
+                                left();
+                            }
+                            break;
+                        case 9:
+                            if (checkEat())
+                            {
+                                state = 9;
+                                go();
+                            }
+                            else
+                            {
+                                state = 10;
+                                go();
+                            }
+                            break;
+                        case 10:
+                            if (checkEat())
+                                state = 11;
+                            else
+                            {
+                                state = 11;
+                                go();
+                            }
+                            break;
+                        case 11:
+                            if (checkEat())
+                            {
+                                state = 3;
+                                go();
+                            }
+                            else
+                                state = 2;
+                            break;
+                        case 12:
+                            if (checkEat())
+                            {
+                                state = 0;
+                                go();
+                            }
+                            else
+                            {
+                                state = 7;
+                                go();
+                            }
+                            break;
+                    }
+
+                    int sleepTime = 0;
+                    this.Invoke((MethodInvoker)delegate() { sleepTime = trackBarSpeed.Value; }); //считываем скорость со слайдера
+                    g.FillRectangle(Brushes.White, 1 + x * 20, 1 + y * 20, 20, 20); //отрисовка муровья
+                    g.DrawRectangle(Pens.Black, 1 + x * 20, 1 + y * 20, 20, 20);
+                    Image img = Resource.ant;
+                    switch (v) //направление картинки
+                    {
+                        case 0:
+                            img.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                            break;
+                        case 1:
+                            img.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                            break;
+                        case 2:
+                            img.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                            break;
+                        case 3:
+                            img.RotateFlip(RotateFlipType.RotateNoneFlipNone);
+                            break;
+                    }
+                    g.DrawImage(img, new Point(1 + x * 20, 1 + y * 20));
+                    Thread.Sleep(sleepTime);
+                }
+            }
+            catch (Exception) { };
+        }
+
+        private bool checkEat() //проверяем наличие яблока перед муравьем
+        {
+            int cpX = 0, cpY = 0; //координаты перед муравьем 
+            switch (v) //в зависимости от направления
+            {
+                case 0:
+                    cpX = x + 1;
+                    if (cpX > 31)
+                        cpX = 0;
+                    cpY = y;
+                    break;
+                case 1:
+                    cpY = y + 1;
+                    if (cpY > 31)
+                        cpY = 0;
+                    cpX = x;
+                    break;
+
+                case 2:
+                    cpX = x - 1;
+                    if (cpX < 0)
+                        cpX = 31;
+                    cpY = y;
+                    break;
+
+                case 3:
+                    cpY = y - 1;
+                    if (cpY < 0)
+                        cpY = 31;
+                    cpX = x;
+                    break;
+            }
+            if (tor[cpY, cpX] == 1)
+                return true;
+            return false;
+        }
+
+        private void left() //повернуть налево
+        {
+            v--;
+            if (v < 0)
+                v = 3;
+        }
+
+        private void right() //повернуть напрво
+        {
+            v++;
+            if (v > 3)
+                v = 0;
+        }
+
+        private void go() 
+        { //идти вперед
+            switch (v) //в зависимости от направления
+            {
+                case 0:
+                    x++;
+                    if (x > 31)
+                        x = 0;
+                    break;
+                case 1:
+                    y++;
+                    if (y > 31)
+                        y = 0;
+                    break;
+                case 2:
+                    x--;
+                    if (x < 0)
+                        x = 31;
+                    break;
+                case 3:
+                    y--;
+                    if (y < 0)
+                        y = 31;
+                    break;
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            work = false; //если закрылось окно остановить поток
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            PaintTor(e.Graphics); //перерисовка тора
+        }
+    }
+}
